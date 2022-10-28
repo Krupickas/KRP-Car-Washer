47995585f96eaff9ec582bff28fb4176c5f01fc9
Krupickas <114585256+Krupickas@users.noreply.github.com>
2022-10-28 19:39:50 +0200

    Add files via upload

---
+++ krp_carwash/config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+
+Config.BlipSprite = 100
+Config.BlipScale = 0.6
+Config.BlipName = 'Car Wash'
+
+
+Config.Blips = {
+    [1] = {["x"] = -698.9388, ["y"] = -931.9453, ["z"] = 77.9189, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Car Washer"},
+    [2] = {["x"] = 24.9860, ["y"] = -1392.2036, ["z"] = 29.3339, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Car Washer"},
+    [3] = {["x"] = 170.4428, ["y"] = -1718.6241, ["z"] = 29.3016, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Car Washer"},
+    [4] = {["x"] = 1365.0729, ["y"] = 3604.1377, ["z"] = 34.8182, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Car Washer"},
+    [5] = {["x"] = 1698.3536, ["y"] = 4916.9810, ["z"] = 42.0781, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Car Washer"},
+    [6] = {["x"] = -216.5613, ["y"] = 6216.5005, ["z"] = 31.4910, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Car Washer"},
+}
