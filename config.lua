818b6d6d931637b96ac8b134e10c423fcea777bc
Nema <petermannfelipe@gmail.com>
2022-10-29 06:05:37 -0300

    refactor(sv-cl-config)

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Duration = 10000
+Config.Amount = 2000
+
+Config.Blips = {
+    [1] = {["x"] = -698.9388, ["y"] = -931.9453, ["z"] = 77.9189, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Lavado de autos"},
+    [2] = {["x"] = 24.9860, ["y"] = -1392.2036, ["z"] = 29.3339, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Lavado de autos"},
+    [3] = {["x"] = 170.4428, ["y"] = -1718.6241, ["z"] = 29.3016, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Lavado de autos"},
+    [4] = {["x"] = 1365.0729, ["y"] = 3604.1377, ["z"] = 34.8182, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Lavado de autos"},
+    [5] = {["x"] = 1698.3536, ["y"] = 4916.9810, ["z"] = 42.0781, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Lavado de autos"},
+    [6] = {["x"] = -216.5613, ["y"] = 6216.5005, ["z"] = 31.4910, ["id"] = 100, ["color"] = 0, ["scale"] = 0.5, ["text"] = "Lavado de autos"},
+}
